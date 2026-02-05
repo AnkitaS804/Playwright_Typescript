37c2a1228e2770124ce2bbe2c851855987496415
AnkitaS804 <ankita.show@capgemini.com>
2026-02-05 12:12:36 +0530

    first commit

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,8 +1,10 @@
-import { test } from '@playwright/test';
+import { test,expect } from '@playwright/test';
 
 test("test1",async({page})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     await page.getByPlaceholder("Username").fill("Admin")
     await page.getByPlaceholder("Password").fill("admin123")
     await page.getByRole('button',{name:"submit"})
+    await expect(page).toHaveTitle('OrangeHRM')
+
 })
