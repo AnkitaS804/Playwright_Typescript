c9eb374fb4ed79697338372f355410a5a3db259f
AnkitaS804 <ankita.show@capgemini.com>
2026-02-05 12:40:43 +0530

    update

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -6,6 +6,4 @@ test("test1",async({page})=>{
     await page.getByPlaceholder("Password").fill("admin123")
     await page.getByRole('button',{name:"submit"})
     await expect(page).toHaveTitle('OrangeHRM')
-    
-
 })
