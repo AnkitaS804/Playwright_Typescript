d1f44baa5c028925d2d6a062e10027ae9a23f05b
AnkitaS804 <ankita.show@capgemini.com>
2026-02-05 11:55:27 +0530

    first commit

---
+++ tests/test.spec.ts
@@ -0,0 +1,8 @@
+import { test } from '@playwright/test';
+
+test("test1",async({page})=>{
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    await page.getByPlaceholder("Username").fill("Admin")
+    await page.getByPlaceholder("Password").fill("admin123")
+    await page.getByRole('button',{name:"submit"})
+})
