1980fc697dfe3a2a7aab46737a67898d01f41013
AnkitaShow <ankita.show@capgemini.com>
2026-04-01 12:02:50 +0530

    commit message

--- tests/test2.spec.ts
+++ tests/test2.spec.ts
@@ -1,1 +1,10 @@
-//test2
+import { test,expect } from '@playwright/test';
+
+test("test2", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await page.getByLabel("Username:").fill("rahulshettyacademy");
+  await page.getByLabel("Password:").fill("Learning@830$3mK2");
+  await page.getByLabel("I Agree to the terms and conditions").check();
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await expect(page).toHaveTitle("ProtoCommerce");
+});
