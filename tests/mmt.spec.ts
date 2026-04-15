9164c4a8df6f28d980aa190559f3b13cdced57a1
AnkitaShow <ankita.show@capgemini.com>
2026-04-15 12:07:08 +0530

    commit

--- tests/mmt.spec.ts
+++ tests/mmt.spec.ts
@@ -1,26 +1,26 @@
-import { test } from '@playwright/test';
-import { MmtPage } from '../page/mmtPage';
-import testData from '../testdata/flightdata.json';
+// import { test } from '@playwright/test';
+// import { MmtPage } from '../page/mmtPage';
+// import testData from '../testdata/flightdata.json';
 
-testData.forEach((data) => {
-  test(`Flight search from ${data.fromCity} to ${data.toCity}`, async ({ page }) => {
-    const mmt = new MmtPage(page);
-    await mmt.goto();
+// testData.forEach((data) => {
+//   test(`Flight search from ${data.fromCity} to ${data.toCity}`, async ({ page }) => {
+//     const mmt = new MmtPage(page);
+//     await mmt.goto();
 
-    await mmt.closePopUp();
-    await mmt.openFlightsTab();
-    await mmt.selectRoundTrip();
-    await mmt.verifyHomePage();
+//     await mmt.closePopUp();
+//     await mmt.openFlightsTab();
+//     await mmt.selectRoundTrip();
+//     await mmt.verifyHomePage();
 
-    await mmt.fillFrom(data.fromCity);
-    await mmt.fillTo(data.toCity);
+//     await mmt.fillFrom(data.fromCity);
+//     await mmt.fillTo(data.toCity);
 
-    await mmt.selectDepartureDate(data.departureDate);
-    await mmt.clickNextMonth();
-    await mmt.selectReturnDate(data.returnDate);
+//     await mmt.selectDepartureDate(data.departureDate);
+//     await mmt.clickNextMonth();
+//     await mmt.selectReturnDate(data.returnDate);
 
-    await mmt.configurePassengers();
-    await mmt.searchFlights();
-  });
-});
+//     await mmt.configurePassengers();
+//     await mmt.searchFlights();
+//   });
+// });
 
