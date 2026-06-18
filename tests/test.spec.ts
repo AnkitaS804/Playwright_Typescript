a501a9ac3d153c08fb082f5804b8556ce9bbc5b6
AnkitaShow <ankita.show@capgemini.com>
2026-06-18 12:44:52 +0530

    commit

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -4,7 +4,13 @@ test("test1",async({page})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     await page.getByPlaceholder("Username").fill("Admin")
     await page.getByPlaceholder("Password").fill("admin123")
-    await page.getByRole('button',{name:"submit"})
+    await page.getByRole('button',{name:"login"}).click()
     await expect(page).toHaveTitle('OrangeHRM')
+    
+    const timeLink = page.getByRole('link', { name: 'Time' });
+    await expect(timeLink).toBeVisible();
+    await timeLink.click();
 
-})
+    await expect(page.getByText('Select Employee')).toBeVisible();
+});
+
